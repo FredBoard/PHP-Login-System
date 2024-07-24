7fd9c99bbec6d359759a9abc92c382fc121deca9
Fred <91249604+FredBoard@users.noreply.github.com>
2024-07-24 07:30:08 +0200

    Update for PHP 8.2

--- assets/setup/DBcreation.sql
+++ assets/setup/DBcreation.sql
@@ -1,35 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : mer. 24 juil. 2024 à 07:18
+-- Version du serveur : 10.11.6-MariaDB
+-- Version de PHP : 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `klik_loginsystem`
+-- Base de données : `klik_loginsystem`
 --
-
-create schema klik_loginsystem;
-use klik_loginsystem;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auth_tokens`
+-- Structure de la table `auth_tokens`
 --
 
 CREATE TABLE `auth_tokens` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `auth_type` varchar(255) NOT NULL,
   `selector` text NOT NULL,
   `token` longtext NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `expires_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `auth_tokens`
+--
+
+INSERT INTO `auth_tokens` (`id`, `user_email`, `auth_type`, `selector`, `token`, `created_at`, `expires_at`) VALUES
+(1, 'toto@noname.com', 'password_reset', '6646c9ca6b77096a', '$2y$10$aOTPuajK18ySQY9K2AADCuQAJaiurANyZGE1rd8vOnNzSZlCXb9eC', '2024-07-24 05:15:41', '2024-07-23 18:04:53'),
+(2, 'titi@noname.com', 'account_verify', 'c4b70a21e60286aa', '$2y$10$vJ3lrcneIiAgTA5ZhrMCSOj0qd/5RkApmyAFV0OKO8XaKiL1WIIDq', '2024-07-24 05:15:45', '2024-07-23 18:40:15');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -43,43 +66,56 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deleted_at` timestamp NULL DEFAULT NULL,
-  `last_login_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `last_login_at` timestamp NULL DEFAULT NULL,
+  `user_level` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 --
--- Dumping data for table `users`
+-- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`username`, `email`, `password`, `first_name`, `last_name`, `gender`, `headline`, `bio`, `created_at`) VALUES
-('supahot', 'supa@hot.com', '$2y$10$jhIOk4NVdBile/NwhAU9We/f0aoohx.cG9CizmIALRz0aCKJa5s6a', 'Supahot', 'Soverysupahot', 'm', 'Headline of a supa hot user', 'This is the bio of a supa hot user. Now i will say needless stuff to make this longer so this looks like a bio and not anything other than a bio.', NOW());
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `gender`, `headline`, `bio`, `profile_image`, `verified_at`, `created_at`, `updated_at`, `deleted_at`, `last_login_at`, `user_level`) VALUES
+(1, 'supahot82', 'titi@noname.com', '$2y$10$dE9ouzRrCI5pot0OC849euAfG5pEJFokrBnqPx6L2WjisBwvKSFG.', NULL, '', NULL, '', '', '_defaultUser.png', '2024-07-24 05:11:17', '2024-07-24 05:10:20', '2024-07-24 05:17:19', NULL, '2024-07-24 05:10:59', 0),
+(2, 'Fred', 'toto@noname.com', '$2y$10$ZVXUh.OMvLJST3FQQpcjoeiPRZa7TkldeMhkFStHgIBZSz/pEFboC', NULL, '', NULL, '', '', '_defaultUser.png', '2024-07-23 17:43:48', '2024-07-23 17:43:27', '2024-07-24 05:17:25', NULL, '2024-07-23 17:44:01', 0);
 
 --
--- Indexes for table `auth_tokens`
+-- Index pour les tables déchargées
 --
 
+--
+-- Index pour la table `auth_tokens`
+--
 ALTER TABLE `auth_tokens`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- Indexes for table `users`
+-- Index pour la table `users`
 --
-
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `id` (`id`,`username`,`email`);
+  ADD UNIQUE KEY `id_2` (`id`,`username`,`email`);
 
 --
--- AUTO_INCREMENT for table `auth_tokens`
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `auth_tokens`
 --
 ALTER TABLE `auth_tokens`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
